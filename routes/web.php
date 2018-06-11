33f8425181e1a9f0b0538ec51f9af7dad08d7107
masahiro.ozaki <masahiro.b.ozaki@rakuten.com>
2018-06-11 06:17:13 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', 'TasksController@index');
+// Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
+
+// user registration
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
